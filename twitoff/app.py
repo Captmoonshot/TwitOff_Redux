c7c5d9c88f629ba768f55d866c397dc63a408a62
Sammy Lee <sammylee@Sammys-MacBook-Air.local>
2019-03-05 10:04:23 -0800

    added templates

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,6 @@
 """Main application and routing logic for TwitOff."""
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
 
 
 
@@ -8,11 +8,14 @@ def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = 'Debug'    # TODO change before deploying
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to TwitOff!'
+    	users = User.query.all()
+    	return render_template('base.html', title='Home', users=users)
 
     return app
 
